1caf090bad156607e546696465d763fa23917f83
HELENA CARRILLO <patty99_helena@hotmail.com>
2023-06-29 05:57:25 -0600

    trigger

--- TAREA8.md
+++ TAREA8.md
@@ -31,13 +31,10 @@ Lo mismo para el número de victorias:
     SELECT class, playstyle, round(sum(win)) AS total_wins FROM championsstats 
 	    GROUP BY class, playstyle ORDER BY total_wins DESC;
 
-## Left Join
 
 ## Join
 
-## Subconsulta
-
-## Trigger
+## Trigger, Left Join y Subconsulta
 
 Se intetó crear una vista con la indicación:
 
@@ -50,3 +47,64 @@ Se intetó crear una vista con la indicación:
 
 Sin embargo se registró un error ya que ambas columnas tienen el mismo nombre por lo que se modificaron los nombres de la tabla *player_def*:
 
+    ALTER TABLE player_def RENAME COLUMN player_name TO player_name_d;
+    ALTER TABLE player_def RENAME COLUMN wins TO wins_d;
+    ALTER TABLE player_def RENAME COLUMN loses TO loses_d;
+    ALTER TABLE player_def RENAME COLUMN win_rate TO win_rate_d;
+
+Una vez cambiados los nombre se puede realizar con exito el LEFT JOIN:
+
+    CREATE VIEW jugadores AS
+		SELECT * FROM player_stats AS s
+	 	LEFT JOIN player_def AS d ON s.player_name = d.player_name_d;
+
+Una vez comprobado que no existen problemas al momento de crear vistas con este tipo JOIN se procede a crear una nueva consulta que me permita actualizar la tabla *player_def*.
+
+Para esto se verifica primero el resultado para la consulta:
+
+    SELECT player_name,
+		COUNT(DISTINCT season) AS sseason,
+		SUM(wins) AS swins,
+		SUM(games_played) AS sgames_played,
+		SUM(loses) AS sloses FROM player_stats GROUP BY player_name;
+
+Esta consulta devuelve un resumen de las estadísticas de cada jugador. En escencia esta es la información encontrada en *player_def*.
+
+Al comprobar que la consulta funciona se utiliza para crear una vista que una ambas tablas:
+
+    DROP VIEW IF EXISTS jugadores;
+
+    CREATE VIEW jugadores AS
+	    SELECT * FROM (
+	     SELECT player_name,
+		    COUNT(DISTINCT season) AS sseason,
+		    SUM(wins) AS swins,
+		    SUM(games_played) AS sgames_played,
+		    SUM(loses) AS sloses 
+            FROM player_stats GROUP BY player_name
+        ) AS s
+	    LEFT JOIN player_def AS d ON s.player_name = d.player_name_d;
+
+La vista jugadores cuenta con información resumida de los jugadores desde dos origenes: la almacenada en *player_def* y la que se obtiene directo de *player_stats*.
+
+Considerando que al momento de actualizar la base de datos se hará desde *player_stats* se crea un TRIGGER que me permita actualizar *player_def* a través de la vista *jugadores*.
+
+    DELIMITER //
+
+    CREATE TRIGGER actualizar_players_def AFTER INSERT ON player_stats
+    FOR EACH ROW
+     BEGIN
+	    UPDATE jugadores
+		    SET jugadores.seasons_played = jugadores.sseasons WHERE    jugadores.sseason > jugadores.seasons_played;
+	    UPDATE jugadores
+		    SET jugadores.wins_d = jugadores.swins WHERE jugadores.swins > jugadores.wins_d;
+	    UPDATE jugadores
+		    SET jugadores.played_match = jugadores.sgames_played WHERE jugadores.sgames_played > jugadores.played_match;
+	    UPDATE jugadores
+		    SET jugadores.loses_d = jugadores.sloses WHERE jugadores.sloses > jugadores.loses_d;
+	    UPDATE jugadores 
+		    SET win_rate_d = jugadores.swins / jugadores.sgames_played;
+	    END;
+    //
+
+    DELIMITER ;
---
+++ TAREA8script.sql
@@ -0,0 +1,110 @@
+USE lol_stats;
+
+SHOW tables;
+
+--- RIGHT Joins ensures ONLY champs WITH stats FROM MATCH will appear
+
+DROP VIEW IF EXISTS championsstats;
+CREATE VIEW championsstats AS
+	SELECT * FROM champion_def AS iz
+	RIGHT JOIN champion_m_stats AS de
+	ON iz.name_champ = de.champ_name;
+
+SELECT class, round(sum(kills)) AS total_kills FROM championsstats 
+	GROUP BY class ORDER BY total_kills DESC;
+
+SELECT class, playstyle, round(sum(kills)) AS total_kills FROM championsstats 
+	GROUP BY class, playstyle ORDER BY total_kills DESC;
+
+SELECT class, round(sum(win)) AS total_wins FROM championsstats 
+	GROUP BY class ORDER BY total_wins DESC;
+SELECT class, playstyle, round(sum(win)) AS total_wins FROM championsstats 
+	GROUP BY class, playstyle ORDER BY total_wins DESC;
+
+
+---- TRIGGER, LEFT JOIN y subconsulta
+
+--- primer intento de vistas y LEFT JOIN
+
+DROP VIEW IF EXISTS jugadores;
+CREATE VIEW jugadores AS
+		SELECT * FROM player_stats AS s
+	 	LEFT JOIN player_def AS d ON s.player_name = d.player_name_d;
+	 
+	 
+--- cambio de nombres para solucionar errores de mismo nombre
+ALTER TABLE player_def RENAME COLUMN player_name TO player_name_d;
+ALTER TABLE player_def RENAME COLUMN wins TO wins_d;
+ALTER TABLE player_def RENAME COLUMN loses TO loses_d;
+ALTER TABLE player_def RENAME COLUMN win_rate TO win_rate_d;
+
+	 
+
+--- Subconsulta para obtener sumatorias de stats e ingresar en jugadores 
+SELECT player_name,
+		COUNT(DISTINCT season) AS sseason,
+		SUM(wins) AS swins,
+		SUM(games_played) AS sgames_played,
+		SUM(loses) AS sloses FROM player_stats GROUP BY player_name;
+	 
+	 
+	 
+	 -- CREA jugadores
+DROP VIEW IF EXISTS jugadores;
+
+CREATE VIEW jugadores AS
+	SELECT * FROM (
+	 SELECT player_name,
+		COUNT(DISTINCT season) AS sseason,
+		SUM(wins) AS swins,
+		SUM(games_played) AS sgames_played,
+		SUM(loses) AS sloses FROM player_stats GROUP BY player_name) AS s
+	 LEFT JOIN player_def AS d ON s.player_name = d.player_name_d;
+	 
+
+SELECT * FROM jugadores;
+
+-- CREAR TRIGGER
+DROP TRIGGER IF EXISTS actualizar_players_def;
+
+-- DELIMITER //
+CREATE TRIGGER actualizar_players_def AFTER INSERT ON player_stats
+FOR EACH ROW
+ BEGIN
+	UPDATE jugadores
+		SET jugadores.seasons_played = jugadores.sseasons WHERE jugadores.sseason > jugadores.seasons_played;
+	UPDATE jugadores
+		SET jugadores.wins_d = jugadores.swins WHERE jugadores.swins > jugadores.wins_d;
+	UPDATE jugadores
+		SET jugadores.played_match = jugadores.sgames_played WHERE jugadores.sgames_played > jugadores.played_match;
+	UPDATE jugadores
+		SET jugadores.loses_d = jugadores.sloses WHERE jugadores.sloses > jugadores.loses_d;
+	UPDATE jugadores 
+		SET win_rate_d = jugadores.swins / jugadores.sgames_played;
+	END;
+-- //
+-- DELIMITER ;
+
+/* PRUEBAS PARA SUBCONSULTA
+	
+-- SELECT * FROM jugadores;
+
+SELECT player_name, COUNT(DISTINCT season) FROM jugadores GROUP BY player_name; -- temporadas jugadas
+SELECT player_name, SUM(wins) FROM jugadores GROUP BY player_name; -- wins
+SELECT player_name, SUM(games_played) FROM jugadores GROUP BY player_name; -- juego jugados
+SELECT player_name, SUM(loses) FROM jugadores GROUP BY player_name;
+
+SELECT player_name, AVG(played_match), SUM(games_played) FROM jugadores GROUP BY player_name; -- juego jugados */
+
+
+/* CORRECCIONES ENCONTRADAS AL MOMETO DE TRABAJAR LOS DATOS
+ 
+--- Correcciones encontradas al realizar joins en champion_m_stats y champion_def
+
+UPDATE champion_def SET class = 'Controller' WHERE class = 'Controllerer';
+
+UPDATE champion_def SET class = 'Mage' WHERE class = 'Mage Artillery';
+
+UPDATE champion_def SET class = 'Controller' WHERE class= 'Support';
+*/
+
